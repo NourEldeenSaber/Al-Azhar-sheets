72dcd0ad8323c407c11c35141f1c2aac1b113df3
Nour <nour01saber@gmail.com>
2023-10-03 15:21:00 +0300

    L. The Brothers

---
+++ sheet1/L. The Brothers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>  
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string F1, L1, F2, L2;
+
+	cin >> F1 >> L1 >> F2 >> L2;
+
+	if (L1 == L2) {
+		cout << "ARE Brothers";
+	}
+	else {
+		cout << "NOT";
+	}
+	
+	return 0;
+
+}
